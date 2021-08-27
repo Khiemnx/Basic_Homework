6db9f95aa1ad8cd83ccd2401d73a086bb34a0f97
Khiemnx <89245303+Khiemnx@users.noreply.github.com>
2021-08-27 09:55:21 +0700

    fix code homework.php

--- basic_PHP/homework.php
+++ basic_PHP/homework.php
@@ -1,28 +1,23 @@
 <?php 
-
-// $a = array(6, 2, 5, 10, 11, 1, 17, 20);
-// sort($a);
-// echo "Min=".$a[0]."<br>";
-// rsort($a);
-// echo "Max=".$a[0];
-
 //--------------------Homework 1------------------------
+
 $sum = 0;
-$number = 0;
-while($number <= 50) {
-    if($number % 2 == 1) $sum +=$number;
-    $number++;
-};
+for($i = 0 ; $i <= 50 ; $i++){
+    if($i % 2 == 1) {
+        $sum += $i;
+    }
+}
+
 echo "the sum of the odd numbers between 0 to 50: ".$sum."<br>";
 
 ///////
 
 //--------------------Homework 2------------------------
+
 function factorialOfNumber($n)
 {
     $result = 1;
-  // Write your source code here
-    for($i = 1; $i <= $n; $i++) {
+    for($i = 1; $i < $n + 1; $i++) {
         $result *= $i;
     }
     return $result;
@@ -35,31 +30,40 @@ echo factorialOfNumber(10)."<br>";
 //--------------------Homework 3------------------------
 
 function check_PrimeNumber($inputNumber){
-    if($inputNumber < 2) return false;
+    if($inputNumber < 2) {
+        return false;
+    }
     if($inputNumber > 2) {
-        for($i = 2 ; $i < $inputNumber/2 ; $i++) {
-            if($inputNumber%$i==0) return false;
+        if($inputNumber % 2 == 0) {
+            return false;
+        } 
+        else {
+            for($i = 3 ; $i < $inputNumber / 2 ; $i += 2) {
+                if($inputNumber % $i == 0 ) {
+                    return false;
+                }
+            }
         }
     }
     return true;
 };
 
-// $arr = array(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97);
-// foreach($arr as $x) {
-//     echo check_PrimeNumber($x)."<br>";
-// }
-
 ////////
 
 //--------------------Homework 4------------------------
 //Use function from Homework 3
+
 function PrimeNumber_inRange($n) {
+    $arr = array();
     while($n > 1) {
-        if(check_PrimeNumber($n)) echo $n."<br>";
+        if(check_PrimeNumber($n)) {
+            array_push($arr, $n);
+        };
         $n--;
     }
+    return $arr;
 }
 
-//PrimeNumber_inRange(100);
+var_dump(PrimeNumber_inRange(100));
 
 /////////
